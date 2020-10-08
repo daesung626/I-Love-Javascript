db0c0ce8c745e91f9ef9023db677e39374af0b42
daesung626 <57124915+daesung626@users.noreply.github.com>
2020-10-07 18:09:16 -0700

    Basic functions of js

---
+++ script.js
@@ -0,0 +1,49 @@
+// console.log('Hello World')
+
+// firstName = 'Daesung';
+// age = 23;
+// job = 'student';
+
+// alert(firstName + ' is a ' + job);
+
+// var lastName = prompt(`What is Daesung's last name?`);
+// console.log(firstName + ' ' + lastName);
+
+// // TERNARY OPERATOR
+
+// var firstName = 'Daesung';
+// var age = 23;
+
+// age >= 21 ? console.log(firstName + ' drinks beer.') : console.log(firstName + ' drinks juice.');
+
+// var drink = age >= 21 ? 'beer' : 'juice';
+// console.log(drink);
+
+// // SWITCH STATEMENT
+
+// var job = 'gambler';
+// switch (job) {
+//     case 'student':
+//         console.log(firstName + ' teaches.');
+//         break;
+//     case 'driver':
+//         console.log(firstName + ' drives.');
+//         break;
+//     case 'gambler':
+//         console.log(firstName + ' is an idiot.');
+//         break;
+//     default:
+//         console.log('This is default');
+// } 
+
+// // EQUALITY COERCION
+
+// var num = 21;
+
+// if (num == '21') {
+//     console.log('==')
+// }
+// if (num === 21) {
+//     console.log('===')
+// }
+
